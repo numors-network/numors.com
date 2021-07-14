7c0605ff586012b3fb1d8471bb3d7d7d582373a5
Mukesh Rana <51038013+numors@users.noreply.github.com>
2021-07-14 16:19:10 +0530

    Update numors.js

---
+++ inc/numors.js
@@ -0,0 +1,11 @@
+var imported = document.createElement("script");
+imported.src = "https://www.googletagmanager.com/gtag/js?id=G-E8N3JS78CM";
+document.head.appendChild(imported);
+
+window.dataLayer = window.dataLayer || [];
+function gtag() {
+  dataLayer.push(arguments);
+}
+gtag("js", new Date());
+
+gtag("config", "G-E8N3JS78CM");
